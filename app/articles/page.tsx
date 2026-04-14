275b8332c3f04cb442e28450fcc2886545f8734d
bijay-develops <bijaybk307@gmail.com>
2026-04-14 23:57:38 +0545

    style: Refactor SVG icons for improved readability and consistency

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,10 +1,9 @@
-
 import { getAllPosts } from "../../lib/posts";
 import ArticlesClient from "./ArticlesClient";
 
 export default function AllArticles() {
-    const posts = getAllPosts();
-    // console.log('this is posts = ', posts);
+  const posts = getAllPosts();
+  // console.log('this is posts = ', posts);
 
-    return <ArticlesClient posts={posts} />;
+  return <ArticlesClient posts={posts} />;
 }
