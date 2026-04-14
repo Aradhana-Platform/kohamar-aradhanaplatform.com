492cb3053e4f8705f142f431d7c1345879d5f84b
bijay-develops <bijaybk307@gmail.com>
2026-04-15 00:02:25 +0545

    style: Refactor SingleSongClient and page components for improved readability and consistency

--- app/songs/page.tsx
+++ app/songs/page.tsx
@@ -2,6 +2,6 @@ import { getAllSongs } from "../../lib/songs";
 import SongsClient from "./SongsClient";
 
 export default async function SongsPage() {
-    const songs = getAllSongs();
-    return <SongsClient initialVideos={songs} />;
+  const songs = getAllSongs();
+  return <SongsClient initialVideos={songs} />;
 }
